6bc0fb6fbd1c6129bbeffdfae6aee30304551ecd
Tatiana <t.ya14091984@gmail.com>
2026-02-16 21:07:13 +0200

    Add initial UI components and layout structures

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,22 @@
+import Home from "./Home.jsx";
+import AboutMe from "./AboutMe.jsx";
+import Contact from "./Contact.jsx";
+import StarWars from "./StarWars.jsx";
+import {navItems} from "../utils/constants.js";
 
-import Hero from "./Hero.jsx";
-import DreamTeam from "./DreamTeam.jsx";
-import OpeningCrawl from "./OpeningCrawl.jsx";
-
-const Main = () => {
-    return (
-        <main className="clearfix">
-            <Hero/>
-            <DreamTeam/>
-            <OpeningCrawl/>
-        </main>
-    );
+const Main = ({page}) => {
+    switch (page) {
+        case navItems[0]:
+            return <Home/>;
+        case navItems[1]:
+            return <AboutMe/>;
+        case navItems[3]:
+            return <Contact/>;
+        case navItems[2]:
+            return <StarWars/>;
+        default:
+            return <Home/>;
+    }
 };
 
 export default Main;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 import NavItem from "./NavItem.jsx";
 import {navItems} from "../utils/constants.js";
 
-const Navigation = () => {
+const Navigation = ({changePage}) => {
     return (
         <nav className="fixed-top mt-2 ms-5">
             <ul className="nav gap-2">
-                {navItems.map(navItem => <NavItem key={navItem} itemTitle={navItem} />)}
+                {navItems.map(navItem => <NavItem changePage={changePage} key={navItem} itemTitle={navItem}/>)}
             </ul>
         </nav>
     );
