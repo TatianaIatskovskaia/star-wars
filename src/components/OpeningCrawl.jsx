7a69b88600acf9a78b5ff39c516d1b34158de927
Tatiana <t.ya14091984@gmail.com>
2026-02-20 19:05:33 +0200

    feat(storages): persist OpeningCrawl to session storage to optimize fetch operations

--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -2,15 +2,20 @@ import {useEffect, useState} from "react";
 import {base_url} from "../utils/constants.js";
 
 const OpeningCrawl = () => {
-    const [openingCrawl, setOpeningCrawl] = useState(null);
+    const [openingCrawl, setOpeningCrawl] = useState(() => sessionStorage.getItem('opening_crawl'));
 
     useEffect(() => {
-        const episode = Math.floor(Math.random() * 6) + 1;
-        fetch(`${base_url}/v1/films/${episode}`)
-            .then(res => res.json())
-            .then(data => setOpeningCrawl(data.opening_crawl))
-            .catch(() => setOpeningCrawl(`Error loading crawl`));
-    }, [])
+        if (!openingCrawl) {
+            const episode = Math.floor(Math.random() * 6) + 1;
+            fetch(`${base_url}/v1/films/${episode}`)
+                .then(res => res.json())
+                .then(data => {
+                    setOpeningCrawl(data.opening_crawl);
+                    sessionStorage.setItem('opening_crawl', data.opening_crawl);
+                })
+                .catch(() => setOpeningCrawl(`Error loading crawl`));
+        }
+    }, [openingCrawl])
 
     if (openingCrawl) {
         return (
